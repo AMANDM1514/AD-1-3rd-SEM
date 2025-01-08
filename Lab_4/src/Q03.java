188efd62ec2f7394b11ef61c74ac92bc57cb28c9
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-01-09 02:36:56 +0530

    Add files via upload

---
+++ Lab_4/src/Q03.java
@@ -0,0 +1,30 @@
+public class Q03 {
+    public void SelectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+        Q03 ob = new Q03();
+        ob.SelectionSort(arr);
+        System.out.print("Sorted Array: ");
+        printArray(arr);
+    }
+}
