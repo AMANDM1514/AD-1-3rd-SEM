e0cf64dc7d5323edbd52a63504e0f77521ef93f4
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-01-11 18:32:25 +0530

    Add files via upload

---
+++ Lab_6/src/Q03.java
@@ -0,0 +1,26 @@
+public class Q03 {
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 20;
+        int result = binarySearch(arr, key);
+        System.out.println(result != -1 ? "Key found at index: " + result : "Key not found");
+    }
+}
