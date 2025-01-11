b9e7a4924b79ff929d769b86df753a17916c11ba
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-01-11 18:36:34 +0530

    Add files via upload

---
+++ Lab_7/src/Q01.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class Q01 {
+    public static int findFirstRepeated(int[] arr) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (seen.contains(num)) {
+                return num; // First repeated element
+            }
+            seen.add(num);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 3, 4, 3, 5, 6};
+        int result = findFirstRepeated(arr);
+        System.out.println(result != -1 ? "First repeated element: " + result : "No repeated elements found");
+    }
+}
