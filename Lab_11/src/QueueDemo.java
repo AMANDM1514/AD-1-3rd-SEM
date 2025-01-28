4399dbe51a3f304b52adba358fc09abc811833a5
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-01-28 22:45:48 +0530

    Update QueueDemo.java

--- Lab_11/src/QueueDemo.java
+++ Lab_11/src/QueueDemo.java
@@ -71,8 +71,6 @@ public class QueueDemo {
 
     public static void main(String[] args) {
         Queue queue = new Queue();
-
-        // Test the queue operations
         queue.add(10);
         queue.add(20);
         queue.add(30);
