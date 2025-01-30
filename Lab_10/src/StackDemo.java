7590c7514e10ebc5f2a480aaab035d4ad8e203e0
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-01-30 23:46:08 +0530

    Update StackDemo.java

--- Lab_10/src/StackDemo.java
+++ Lab_10/src/StackDemo.java
@@ -56,8 +56,6 @@ public class StackDemo {
     }
     public static void main(String[] args) {
         Stack stack = new Stack();
-
-
         stack.push(10);
         stack.push(20);
         stack.push(30);
