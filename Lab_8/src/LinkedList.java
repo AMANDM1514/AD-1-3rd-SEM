b31aec2477c5b82cce0bc97cf15b0eefea9da9b8
AMAN DEV MAHATO <amandevv007@gmail.com>
2025-02-01 22:43:59 +0530

    Update LinkedList.java

--- Lab_8/src/LinkedList.java
+++ Lab_8/src/LinkedList.java
@@ -119,7 +119,6 @@ public class LinkedList {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 
-		// Insertion
 		list.insertAtEnd(10);
 		list.insertAtEnd(20);
 		list.insertAtEnd(30);
